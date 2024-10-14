80648138fda51f9bd3ec04b78fca6197fe04ee02
Ali Helmi <ahelmi365@gmail.com>
2024-10-14 14:56:21 +0300

    test other input (date, radio, color, text)

--- cypress/integration/05-input-obstacles.spec.js
+++ cypress/integration/05-input-obstacles.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+/* eslint-disable no-undef */
 
 describe('Input obstacles', () => {
   beforeEach(() => {
@@ -13,34 +14,35 @@ describe('Input obstacles', () => {
   });
 
   it('should control a select input', () => {
-    cy.get('[data-test="select-input"]');
-    cy.get('[data-test="select-result"]');
+    cy.get('[data-test="select-input"]').select('Hulk');
+    cy.get('[data-test="select-result"]').contains('Hulk');
   });
 
   it('should find and control a checkbox input', () => {
-    cy.get('[data-test="checkbox-tomato"]');
+    cy.get('[data-test="checkbox-tomato"]').as('checkbox');
     cy.get('[data-test="checkbox-result"]').contains('(None)');
-    cy.get('[data-test="checkbox-result"]');
+    cy.get('@checkbox').check();
+    cy.get('[data-test="checkbox-result"]').contains('Tomato');
   });
 
   it('should find and control a radio input', () => {
-    cy.get('[data-test="radio-ringo"]');
-    cy.get('[data-test="radio-result"]');
+    cy.get('[data-test="radio-ringo"]').check();
+    cy.get('[data-test="radio-result"]').contains('Ringo');
   });
 
   it('should find and control a color input', () => {
-    cy.get('[data-test="color-input"]');
-    cy.get('[data-test="color-result"]');
+    cy.get('[data-test="color-input"]').invoke('val', '#abcdef').trigger('input');
+    cy.get('[data-test="color-result"]').contains('#abcdef');
   });
 
   it('should find and control a date input', () => {
-    cy.get('[data-test="date-input"]');
-    cy.get('[data-test="date-result"]');
+    cy.get('[data-test="date-input"]').invoke('val', '2011-09-11').trigger('input');
+    cy.get('[data-test="date-result"]').contains('2011-09-11');
   });
 
-  it('should find and control a range input', () => {
-    cy.get('[data-test="range-input"]');
-    cy.get('[data-test="range-result"]');
+  it.only('should find and control a range input', () => {
+    cy.get('[data-test="range-input"]').invoke('val', '9').trigger('input');
+    cy.get('[data-test="range-result"]').contains('9');
   });
 
   it('should find and control a file input', () => {
